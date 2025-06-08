4e79fc2724c656749a86ff02b646dadd80cbe718
OldLee <2410250284@qq.com>
2025-06-08 22:16:09 +0800

    08 战场调度雏形

--- scenes/characters/character.gd
+++ scenes/characters/character.gd
@@ -19,62 +19,62 @@ var speed: int
 @onready var character_rect = $Container/CharacterRect
 
 func _ready():
-    if character_data:
-        initialize_from_data(character_data)
-    else:
-        push_error("角色场景 " + name + " 没有分配CharacterData!")
+	if character_data:
+		initialize_from_data(character_data)
+	else:
+		push_error("角色场景 " + name + " 没有分配CharacterData!")
 
 func initialize_from_data(data: CharacterData):
-    # 保存数据引用
-    self.character_data = data
-    
-    # 初始化属性
-    self.character_name = data.character_name
-    self.max_hp = data.max_hp
-    self.current_hp = data.current_hp
-    self.max_mp = data.max_mp
-    self.current_mp = data.current_mp
-    self.attack = data.attack
-    self.defense = data.defense
-    self.speed = data.speed
-    
-    # 更新视觉表现
-    update_visual()
-    
-    print(character_name + " 初始化完毕，HP: " + str(current_hp) + "/" + str(max_hp))
+	# 保存数据引用
+	self.character_data = data
+	
+	# 初始化属性
+	self.character_name = data.character_name
+	self.max_hp = data.max_hp
+	self.current_hp = data.current_hp
+	self.max_mp = data.max_mp
+	self.current_mp = data.current_mp
+	self.attack = data.attack
+	self.defense = data.defense
+	self.speed = data.speed
+	
+	# 更新视觉表现
+	update_visual()
+	
+	print_rich("[color=cyan][b]{0}[/b][/color] 初始化完毕，HP: [color=lime]{1}/{2}[/color]".format([character_name, current_hp, max_hp]))
 
 func update_visual():
-    if name_label:
-        name_label.text = character_name
-    
-    if hp_label:
-        hp_label.text = "HP: " + str(current_hp) + "/" + str(max_hp)
-    
-    if character_rect and character_data:
-        character_rect.color = character_data.color
+	if name_label:
+		name_label.text = character_name
+	
+	if hp_label:
+		hp_label.text = "HP: " + str(current_hp) + "/" + str(max_hp)
+	
+	if character_rect and character_data:
+		character_rect.color = character_data.color
 
 # 战斗相关方法
 func apply_damage(amount: int):
-    current_hp = max(0, current_hp - amount)
-    update_visual()
-    print(character_name + " 受到 " + str(amount) + " 点伤害, 剩余HP: " + str(current_hp))
-    
-    if current_hp == 0:
-        die()
+	current_hp = max(0, current_hp - amount)
+	update_visual()
+	print_rich("[color=cyan][b]{0}[/b][/color] 受到 [color=red]{1}[/color] 点伤害, 剩余HP: [color=lime]{2}[/color]".format([character_name, amount, current_hp]))
+	
+	if current_hp == 0:
+		die()
 
 func heal(amount: int):
-    current_hp = min(max_hp, current_hp + amount)
-    update_visual()
-    print(character_name + " 恢复 " + str(amount) + " 点HP, 剩余HP: " + str(current_hp))
+	current_hp = min(max_hp, current_hp + amount)
+	update_visual()
+	print_rich("[color=cyan][b]{0}[/b][/color] 恢复 [color=green]{1}[/color] 点HP, 剩余HP: [color=lime]{2}[/color]".format([character_name, amount, current_hp]))
 
 func use_mp(amount: int) -> bool:
-    if current_mp >= amount:
-        current_mp -= amount
-        update_visual()
-        return true
-    return false
+	if current_mp >= amount:
+		current_mp -= amount
+		update_visual()
+		return true
+	return false
 
 func die():
-    print(character_name + " 已被击败!")
-    # 在完整游戏中会添加死亡动画和事件
-    modulate = Color(1, 1, 1, 0.5) # 半透明表示被击败
+	print_rich("[color=red][b]{0} 已被击败![/b][/color]".format([character_name]))
+	# 在完整游戏中会添加死亡动画和事件
+	modulate = Color(1, 1, 1, 0.5) # 半透明表示被击败
